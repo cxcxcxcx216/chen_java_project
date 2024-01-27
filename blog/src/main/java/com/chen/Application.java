00944e7bd17e4a070f65ae90ef76d7824dfd983a
cx <473427618@qq.com>
2024-01-27 13:35:29 +0800

    测试提交
    Signed-off-by: cx <473427618@qq.com>

--- blog/src/main/java/com/chen/Application.java
+++ blog/src/main/java/com/chen/Application.java
@@ -3,7 +3,7 @@ package com.chen;
 public class Application {
     public static void main(String[] args) {
         System.out.println("hello");
-        int i = 1;
+        int i = 1789;
         i=89;
         System.out.println(i);
     }
