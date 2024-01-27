529bed034eec9b3f431eb51027e2bf95b80ef661
cx <473427618@qq.com>
2024-01-27 12:48:15 +0800

    测试提交

--- blog/src/main/java/com/chen/Application.java
+++ blog/src/main/java/com/chen/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("hello");
         int i = 1;
-        i=2;
+        i=3;
         System.out.println(i);
     }
 }
