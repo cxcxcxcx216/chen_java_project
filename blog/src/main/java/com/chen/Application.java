315f7578bb2b3cd3fdfa775ca62c89367d2d5879
cxcxcxcx216 <57990523+cxcxcxcx216@users.noreply.github.com>
2024-01-27 12:46:52 +0800

    Update Application.java
    测试

--- blog/src/main/java/com/chen/Application.java
+++ blog/src/main/java/com/chen/Application.java
@@ -4,6 +4,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("hello");
         int i = 1;
+        i=2;
         System.out.println(i);
     }
 }
