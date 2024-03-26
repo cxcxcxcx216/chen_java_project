bbeb432d28b0b86ce1ae9d10a27f0d7a7636c3d5
cx <473427618@qq.com>
2024-03-26 17:30:59 +0800

    测试

--- blog/src/main/java/com/chen/Application.java
+++ blog/src/main/java/com/chen/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("hello");
         int i = 1789;
-        i=8923;
+        i=8920;
         System.out.println(i);
     }
 }
